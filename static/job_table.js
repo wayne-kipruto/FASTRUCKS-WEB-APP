92dea1d2f0ff841636cf0faa3f17d5457e84c912
Wayne Kipruto <wayne.kipruto@strathmore.edu>
2022-11-24 01:07:13 +0300

    final

--- static/job_table.js
+++ static/job_table.js
@@ -22,3 +22,65 @@ const db = getFirestore();
 
 
 
+//Filling the table 
+var job = 0;
+var tbody = document.getElementById("tbody2");
+function AddItemToTable(goodsSelected, deliveredFrom,deliveredTo,dateSelected,vehicleSelected ) {
+  let tr = document.createElement("tr");
+  let td1 = document.createElement('td');
+  let td2 = document.createElement('td');
+  let td3 = document.createElement('td');
+  let td4 = document.createElement('td');
+  let td5 = document.createElement('td');
+  let td6 = document.createElement('td');
+  
+  
+
+  td1.innerHTML = ++job;
+  td2.innerHTML = goodsSelected;
+  td3.innerHTML = deliveredFrom;
+  td4.innerHTML = deliveredTo;
+  td5.innerHTML = dateSelected;
+  td6.innerHTML = vehicleSelected
+  
+
+  tr.appendChild(td1);
+  tr.appendChild(td2);
+  tr.appendChild(td3);
+  tr.appendChild(td4);
+  tr.appendChild(td5);
+ 
+
+  tbody.appendChild(tr);
+}
+
+function AddAllItemToTable(jb) {
+  tbody.innerHTML="";
+
+  job = 0;
+
+
+  jb.forEach(element => {
+    AddItemToTable(
+      element.goodsSelected, 
+      element.deliveredFrom, 
+      element.deliveredTo, 
+      element.dateSelected, 
+      element.vehicleSelected,
+      )
+  })
+}
+
+//--------------GET ALL DATA-------------------------//
+async function GetAllDataOnce(){
+  const querySnapshot =await getDocs(collection(db, "jobDetails"));
+  var jobs =[];
+
+  querySnapshot.forEach(doc=>{
+    jobs.push(doc.data());
+  });
+  AddAllItemToTable(jobs);
+}
+
+ window.onload = GetAllDataOnce;
+
